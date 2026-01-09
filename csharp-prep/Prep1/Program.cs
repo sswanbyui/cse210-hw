08c8b1df84d63c023c38f12b22c0e09e95644156
Steve Swan <sswanbyui@byui.edu>
2026-01-08 20:56:34 -0700

    Updated Prep1 and Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName, lastName;
+        
+        Console.Write("What is your first name? ");
+        firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+        //Adding a line break
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int gradePCT, remainder;
+        string letter = "", plusMinus = "";
+        Console.Write("What is your grade percentage? ");
+        gradePCT = int.Parse(Console.ReadLine());
+        remainder = gradePCT % 10;
+
+        if(gradePCT >= 90)
+        {
+            letter = "an A";
+        }
+        if(gradePCT >= 80 && gradePCT < 90)
+        {
+            letter = "a B";
+        }
+        if(gradePCT >= 70 && gradePCT < 80)
+        {
+            letter = "a C";
+        }
+        if(gradePCT >= 60 && gradePCT < 70)
+        {
+            letter = "a D";
+        }
+        if(gradePCT < 60)
+        {
+            letter = "an F";
+        }
+        if(remainder >= 7)
+        {
+            if(gradePCT < 90 && gradePCT >= 60)
+            {
+                plusMinus = "+";
+            }
+            else
+            {
+                plusMinus = "";
+            }
+        }
+        if(remainder <=3)
+        {
+            plusMinus = "-";
+        }
+        Console.WriteLine($"Your grade is {letter}{plusMinus}.");
+        if(gradePCT >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
     }
 }
