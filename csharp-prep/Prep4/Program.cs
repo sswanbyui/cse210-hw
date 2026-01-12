f4284ed55b2a25057cba67bfcc025dc8f25efc94
Steve Swan <sswanbyui@byui.edu>
2026-01-12 14:28:07 -0700

    Program coded and tested according to specifications.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = -1, sum = 0, largest = 0, smallestPositive = 0;
+        double average = 0.0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while(number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if(number !=0)
+            {
+                numbers.Add(number);
+                sum += number;
+                if((number < smallestPositive && number > 0) || smallestPositive == 0)
+                {
+                    smallestPositive = number;
+                }
+            }
+        }
+        if(numbers.Count > 0)
+        {
+            average = sum/numbers.Count;
+        }
+        largest = numbers.Max();
+        numbers.Sort();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        foreach(int i in numbers)
+        {
+            Console.WriteLine($"{i}");
+        }
+
     }
 }
