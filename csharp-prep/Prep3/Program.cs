ff8e7360863d427bb3af7b4c5aefde418f05cb69
Steve Swan <sswanbyui@byui.edu>
2026-01-12 13:24:18 -0700

    Coded and tested Prep 3, per requirements + stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 0, guessNumber = 0, guesses = 0;
+        Random randomGenerator = new Random();
+        magicNumber = randomGenerator.Next(1, 101);
+        while(magicNumber != guessNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+            guesses ++;
+            if(guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Number of guesses: {guesses}");
+                Console.Write("Would you like to play again? ");
+                string playAgain = Console.ReadLine();
+                if(playAgain == "yes")
+                {
+                    magicNumber = randomGenerator.Next(1, 101);
+                    guessNumber = 0;
+                }            
+            }
+        }
     }
 }
